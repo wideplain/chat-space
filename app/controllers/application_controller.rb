078abf21d6fb2eb28b2d28e355d7b9f92fc2480d
KoyaKikuchi <koyakikuchi@wideplain.com>
2017-08-23 21:54:52 +0900

    Sign_up permit name
    サインアップ機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+   before_action :configure＿permitted＿parameters, if: :devise_controller?
+
+  def configure＿permitted＿parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
