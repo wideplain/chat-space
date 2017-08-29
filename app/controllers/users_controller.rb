dc882a0f2d1752b23667459e52e013c67cdbeb79
KoyaKikuchi <koyakikuchi@wideplain.com>
2017-08-29 13:17:25 +0900

    User update
    ユーザー情報更新機能実装。authenticated_user!の実装。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
   def edit
+    @user = current_user
   end
 
   def update
-    user = User.find(params[:id])
-    binding.pry
-    user.update(user_params)
+    @user = User.find(params[:id])
+    @user.update(user_params)
     redirect_to controller: :messages, action: :index
   end
 
   private
   def user_params
-    params.permit(:name, :email).merge(id: current_user.id)
+    params.require(:user).permit(:name, :email).merge(id: current_user.id)
   end
 end
