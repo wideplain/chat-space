eeb45062c6d7fccdf337fbdbc1dd9163e3e53e83
KoyaKikuchi <koyakikuchi@wideplain.com>
2017-09-15 02:54:28 +0900

    correction_view
    ハッシュロケットの修正、エラー表示追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,8 +6,11 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
-    redirect_to root_path
+    if @group.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
