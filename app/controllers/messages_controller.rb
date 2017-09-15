e86bdf5f21bfafcdc628173df46cdad140eec704
KoyaKikuchi <koyakikuchi@wideplain.com>
2017-09-15 13:34:29 +0900

    Includes users
    usersをgroupにincludesした。hamlに改行を施した

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,6 @@
 class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
-    @group = Group.find(params[:group_id])
-    @users = @group.users
+    @group = Group.includes(:users).find(params[:group_id])
   end
 end
