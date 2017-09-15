8d49db52044c6ccce26f232a77cbb67614da6707
KoyaKikuchi <koyakikuchi@wideplain.com>
2017-09-15 16:11:14 +0900

    Message display
    メッセージレコードを表示できるようにした

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
-    @group = Group.includes(:users).find(params[:group_id])
+    @group = Group.includes(:users, :messages).find(params[:group_id])
   end
 end
