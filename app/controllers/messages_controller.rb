708ea8b6fbb69dc003ce4f2e2f8724e2b176aa75
KoyaKikuchi <koyakikuchi@wideplain.com>
2017-09-15 22:37:16 +0900

    Message create
    メッセージを保存するためのストロングパラメータの追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,15 @@ class MessagesController < ApplicationController
     @groups = current_user.groups
     @group = Group.includes(:users, :messages).find(params[:group_id])
   end
+
+  def create
+    @message = Message.create(message_params)
+  end
+
+
+  private
+
+  def message_params
+
+  end
 end
