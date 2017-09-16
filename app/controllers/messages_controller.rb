ac621cc22cc2071c01cb89f3db65a99fd66bdd44
KoyaKikuchi <koyakikuchi@wideplain.com>
2017-09-16 22:19:58 +0900

    message create implemention
    メッセージ投稿、表示、フラッシュ、サイドバーメッセージ表示の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,14 +5,23 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
+  def new
+
+  end
+
   def create
-    @message = Message.create(message_params)
+    if message_params[:body].present?
+      @message = Message.create(message_params)
+      redirect_to group_messages_path, notice: 'メッセージが投稿されました'
+    else
+      redirect_to group_messages_path, alert: 'メッセージを入力してください'
+    end
   end
 
 
   private
 
   def message_params
-
+    params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
