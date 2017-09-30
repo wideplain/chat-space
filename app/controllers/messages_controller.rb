b9dbf1eb277427e822b83ee5436cc1ae62bea3ba
KoyaKikuchi <koyakikuchi@wideplain.com>
2017-10-01 01:07:40 +0900

    Correction message controller and ja yml and time format
    画像投稿は実装していない。ja.ymlのいらない記述を消した。時間のフォーマットのデフォルトを変更した。コントローラーを変えた。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,14 +10,16 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.create(message_params)
-    redirect_to group_messages_path #render :indexだとNoMethodErrorエラーが出る
+    message = Message.new(message_params)
+    if message.save
+      redirect_to group_messages_path
+    end
   end
 
 
   private
 
   def message_params
-    params.require(:message).permit(:body, :user_id).merge(group_id: params[:group_id])
+    params.require(:message).permit(:body, :user_id, :image).merge(group_id: params[:group_id])
   end
 end
